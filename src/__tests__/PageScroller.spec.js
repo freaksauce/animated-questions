3fb9f9f5b9115687d247b2cebb6201b1f0e132ae
Jon <jon.bloomer@iag.com.au>
2017-10-03 16:40:09 +1100

    added new test scenario

--- src/__tests__/PageScroller.spec.js
+++ src/__tests__/PageScroller.spec.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { configure, shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import sinon from 'sinon'
 
+
 import PageScroller from '../PageScroller'
+import Page from '../Page'
 import Component1 from '../Component1'
 import Component2 from '../Component2'
 import Component3 from '../Component3'
 import Component4 from '../Component4'
 
+configure({ adapter: new Adapter() })
+
+const shallowRender = shallow(<PageScroller pages={[Component1, Component3, Component2, Component4]} />)
+const mountComponent = mount(<PageScroller pages={[Component1, Component3, Component2, Component4]} />)
+
 describe('<PageScroller />', () => {
-  it('should render a single page component', () => {
-    const wrapper = shallow(<PageScroller pages={[Component1, Component2, Component3, Component4]} />)
-    expect(wrapper.find(Page)).to.have.length(3)
+  it('should render a page component', () => {
+    expect(shallowRender.find(Page)).toBeTruthy()
+  })
+
+  it('should display "This is page 1" text', () => {
+    expect(mountComponent.find(Page).first().find('label').text()).toBe('This is page 1')
   })
 })
